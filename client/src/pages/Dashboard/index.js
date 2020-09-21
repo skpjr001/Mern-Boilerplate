0cd6e3cbfb2aa125336cd90630ec697f7485cbcd
skpjr001 <sachinkumarskrose@gmail.com>
2020-09-21 14:30:37 +0530

    added markdown support(incomplete)

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,9 +1,11 @@
-import { Accordion, AccordionDetails, AccordionSummary, Button, Card, CardContent, InputLabel, TextareaAutosize, TextField, Typography } from '@material-ui/core'
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import { Button, Card, CardContent, TextField, Typography } from '@material-ui/core'
 import React, { useEffect, useRef, useState } from 'react'
 import { useHistory } from 'react-router-dom';
-import { apiCall } from '../utilities';
+import MarkdownIt from 'markdown-it';
+import MDemoji from 'markdown-it-emoji';
 import './style.css';
+
+
 
 function Dashboard() {
 
@@ -12,8 +14,9 @@ function Dashboard() {
     const [toggleForm, setToggleForm] = useState(false);
     const [newNote, setNewNote] = useState({title:'', note:''});
     const [notesList, setNotesList] = useState([]);
+    const md = new MarkdownIt();
+    md.use(MDemoji);
 
-    const inputRef = useRef(null);
 
     const API_URL = 'http://localhost:1337/world'
 
@@ -35,56 +38,61 @@ function Dashboard() {
                
 	}, []);
 
-
-
-
     // Log Out Handler Function
-    const handleLogOut = (e) => {
+    function handleLogOut() {
         localStorage.removeItem('token');
         console.log('sign out');
         history.push('/login');
     }
 
     // Note Handler Function
-    const handleNote = (e) => {
-        const { name, value} = e.target;
+    function handleNote(e) {
+        const { name, value } = e.target;
         setNewNote(prevState => ({
             ...prevState,
-            [name]:value
+            [name]: value
         }));
     }
 
     // addNote Handler Function
-    const  addNote = async () => {
+    async function addNote() {
         //console.log();
-        const res = await fetch('/api/v1/notes', {
+        await fetch('/api/v1/notes', {
             method: 'POST',
-            body : JSON.stringify(newNote),
+            body: JSON.stringify(newNote),
             headers: {
                 'content-type': 'application/json',
-                authorization : `Bearer: ${localStorage.getItem('token')}`
+                authorization: `Bearer: ${localStorage.getItem('token')}`
             }
         }).then(res => res.json())
-        .then((note) =>{
-            setNotesList([...notesList, note]);
-            //console.log(notesList);
-            setNewNote({title:'',note:''});
-            setToggleForm(false);
-        })
+            .then((note) => {
+                setNotesList([...notesList, note]);
+                //console.log(notesList);
+                setNewNote({ title: '', note: '' });
+                setToggleForm(false);
+            });
     }
     
     // addNote Handler Function
-    const getNotes = () => {
-        fetch('/api/v1/notes', {
-            headers : {
-                authorization : `Bearer: ${localStorage.getItem('token')}`
+    async function getNotes() {
+        await fetch('/api/v1/notes', {
+            headers: {
+                authorization: `Bearer: ${localStorage.getItem('token')}`
             },
         }).then((res) => res.json())
-        .then((notes) => {
-            setNotesList(notes);
-            
-        })
+            .then((notes) => {
+                setNotesList(notes);
+
+            });
     }
+
+    // Function to render Note with Markdown
+    //Broken fix later
+    // function renderMarkDown(note) {
+    //     let html =md.render(note)
+    //     console.log(html)
+    //     return { __html: html };
+    // }
     
 
 
@@ -143,15 +151,17 @@ function Dashboard() {
             >Add note</Button>
 
             </form> : <h1>Toggle to write note</h1>}
+
+            
             <div className='card-wrapper'>
-            {user ? notesList.map((e) => 
-                <Card key={e._id} className='card'>
+            {user ? notesList.map((note) => 
+                <Card key={note._id} className='card'>
                     <CardContent className='card-content'>
                     <Typography color="textSecondary" gutterBottom>
-                        {e.title}
+                        {note.title}
                     </Typography>
-                    <Typography variant="h5" component="h2">
-                        {e.note}
+                    <Typography variant='h6' component="h5">
+                    {note.note}  
                     </Typography>
                     </CardContent>
                 </Card>
